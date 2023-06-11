5efdfd421b8d784b4e5748d5690f6de6f3fa8e86
bdevops99 <bdevops99@gmail.com>
2023-06-11 18:50:32 +0530

    Jenkins - pipeline

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -21,7 +21,7 @@ def call() {
             stage('Code Quality') {
                 steps {
                     sh 'ls -l'
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.91.128:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.91.128:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
                 }
 
             }
