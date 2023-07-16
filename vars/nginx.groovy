419cdf9c282ff59efa7c4ad3184521150d4b7ed3
bdevops99 <bdevops99@gmail.com>
2023-07-16 21:55:08 +0530

    Docker - Kubernetes

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -48,19 +48,31 @@ def call() {
                         env.TAG_NAME ==~ ".*"
                     }
                 }
+//                steps {
+//                    // 'npm install'
+//                    sh 'echo $TAG_NAME >VERSION'
+//                    sh 'zip -r ${component}-${TAG_NAME}.zip *'
+//                    //deleting the file as it is not needed
+//                   // sh 'zip -d ${component}-${TAG_NAME}.zip Jenkinsfile'
+//                   // sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.92.175:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+//                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 193400300103.dkr.ecr.us-east-1.amazonaws.com'
+//                    sh 'docker build -t 193400300103.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
+//                    sh 'docker push 193400300103.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+//                }
                 steps {
-                    // 'npm install'
+                    // sh 'npm install'
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip *'
-                    //deleting the file as it is not needed
-                   // sh 'zip -d ${component}-${TAG_NAME}.zip Jenkinsfile'
-                   // sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.92.175:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 193400300103.dkr.ecr.us-east-1.amazonaws.com'
-                    sh 'docker build -t 193400300103.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
-                    sh 'docker push 193400300103.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+                    // Deleting this file as it is not needed.
+                    sh 'zip -d ${component}-${TAG_NAME}.zip Jenkinsfile'
+                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://3.91.175.12:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+
+                    sh 'echo Release Application'
                 }
 
             }
+
+
 
         }
         post {
--- vars/python.groovy
+++ vars/python.groovy
@@ -50,13 +50,20 @@ def call() {
                         env.TAG_NAME ==~ ".*"
                     }
                 }
+//                steps {
+//                    sh 'echo $TAG_NAME >VERSION'
+//                //    sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt  VERSION ${schema_dir}'
+//                //    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.92.175:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+//                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 193400300103.dkr.ecr.us-east-1.amazonaws.com'
+//                    sh 'docker build -t 193400300103.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
+//                    sh 'docker push 193400300103.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+//                }
                 steps {
                     sh 'echo $TAG_NAME >VERSION'
-                //    sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt  VERSION ${schema_dir}'
-                //    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.92.175:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 193400300103.dkr.ecr.us-east-1.amazonaws.com'
-                    sh 'docker build -t 193400300103.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
-                    sh 'docker push 193400300103.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION ${schema_dir}'
+                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://3.91.175.12:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+
+                    sh 'echo Release Application'
                 }
 
             }
